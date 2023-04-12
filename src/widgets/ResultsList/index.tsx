7e2ef9aab6b51b2389e065d580575699866759a0
Filippo Rivolta <37808229+frivolta@users.noreply.github.com>
2023-04-12 14:16:51 +0200

    finishing styling

--- src/widgets/ResultsList/index.tsx
+++ src/widgets/ResultsList/index.tsx
@@ -1,7 +1,7 @@
 import ItineraryCard from "@/widgets/ItineraryCard";
 import { Itinerary } from "@/types/models/Itinerary";
 import React, { FC, memo } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Loader from "@/components/Loader";
 import { CustomError } from "@/types/common/CustomError";
 import ErrorMessages from "@/components/ErrorMessages";
@@ -15,15 +15,17 @@ const ResultsList: FC<ResultsListProps> = memo(
   ({ itineraries, isLoading, errors }) => {
     return (
       <Row>
-        <Container>
-          {!isLoading &&
-            itineraries.length > 0 &&
-            itineraries.map((itinerary) => (
-              <ItineraryCard itinerary={itinerary} />
-            ))}
-          {isLoading && <Loader />}
-          {errors?.hasError && <ErrorMessages messages={errors.messages} />}
-        </Container>
+        <Col>
+          <Container>
+            {!isLoading &&
+              itineraries.length > 0 &&
+              itineraries.map((itinerary) => (
+                <ItineraryCard itinerary={itinerary} />
+              ))}
+            {isLoading && <Loader />}
+            {errors?.hasError && <ErrorMessages messages={errors.messages} />}
+          </Container>
+        </Col>
       </Row>
     );
   }
